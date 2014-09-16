69269dd2def97e3a80bee406a6eee30373a32d27
gsvr <george@westerncapelabs.com>
2014-09-16 07:51:49 +0200

    proper dummy states for lp & tp

--- src/teacherperformance.js
+++ src/teacherperformance.js
@@ -1,45 +1,53 @@
+var vumigo = require('vumigo_v02');
+var ChoiceState = vumigo.states.ChoiceState;
+var EndState = vumigo.states.EndState;
+var Choice = vumigo.states.Choice;
 
-self.states.add('state_tp_start', function(name) {
-    return new ChoiceState(name, {
-        question: 'Hi there! What do you want to do?',
 
-        choices: [
-            new Choice('next', 'Go to next state'),
-            new Choice('exit', 'Exit')],
+go.tp = {
+    // TeacherPerformance States
 
-        next: function(choice) {
-            if(choice.value === 'next') {
-                return 'state_tp_next';
-            } else {
-                return 'state_exit';
+    state_tp_start: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('next', 'Go to next state'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'next') {
+                    return 'state_tp_next';
+                } else {
+                    return 'state_tp_exit';
+                }
             }
-        }
-    });
-});
+        });
+    },
 
+    state_tp_next: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
 
-self.states.add('state_tp_next', function(name) {
-    return new ChoiceState(name, {
-        question: 'Hi there! What do you want to do?',
+            choices: [
+                new Choice('again', 'Back to beginning'),
+                new Choice('exit', 'Exit')],
 
-        choices: [
-            new Choice('again', 'Back to beginning'),
-            new Choice('exit', 'Exit')],
-
-        next: function(choice) {
-            if(choice.value === 'again') {
-                return 'state_tp_start';
-            } else {
-                return 'state_exit';
+            next: function(choice) {
+                if(choice.value === 'again') {
+                    return 'state_tp_start';
+                } else {
+                    return 'state_tp_exit';
+                }
             }
-        }
-    });
-});
+        });
+    },
 
+    state_tp_exit: function(name) {
+        return new EndState(name, {
+            text: 'Thanks, cheers!',
+            next: 'state_tp_start'
+        });
+    }
 
-self.states.add('state_exit', function(name) {
-    return new EndState(name, {
-                text: 'Thanks, cheers!',
-                next: 'state_tp1'
-            });
-});
+};
