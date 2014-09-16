9da1cc6729fda83b870622544ab98b245e2735f6
gsvr <george@westerncapelabs.com>
2014-09-16 07:54:45 +0200

    compiled go-app

--- go-app.js
+++ go-app.js
@@ -5,36 +5,155 @@
 var go = {};
 go;
 
+var vumigo = require('vumigo_v02');
+var ChoiceState = vumigo.states.ChoiceState;
+var EndState = vumigo.states.EndState;
+var Choice = vumigo.states.Choice;
+
+
+go.lp = {
+    // LearnerPerformance States
+
+    state_lp_start: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('next', 'Go to next state'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'next') {
+                    return 'state_lp_next';
+                } else {
+                    return 'state_lp_exit';
+                }
+            }
+        });
+    },
+
+    state_lp_next: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('to_tp', 'Switch to TeacherPerformance'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'to_tp') {
+                    return 'state_tp_start'; // Switch to TP
+                } else {
+                    return 'state_lp_exit';
+                }
+            }
+        });
+    },
+
+    state_lp_exit: function(name) {
+        return new EndState(name, {
+            text: 'Thanks, cheers!',
+            next: 'state_lp_start'
+        });
+    }
+
+};
+
+var vumigo = require('vumigo_v02');
+var ChoiceState = vumigo.states.ChoiceState;
+var EndState = vumigo.states.EndState;
+var Choice = vumigo.states.Choice;
+
+
+go.tp = {
+    // TeacherPerformance States
+
+    state_tp_start: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('next', 'Go to next state'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'next') {
+                    return 'state_tp_next';
+                } else {
+                    return 'state_tp_exit';
+                }
+            }
+        });
+    },
+
+    state_tp_next: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('again', 'Back to beginning'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'again') {
+                    return 'state_tp_start';
+                } else {
+                    return 'state_tp_exit';
+                }
+            }
+        });
+    },
+
+    state_tp_exit: function(name) {
+        return new EndState(name, {
+            text: 'Thanks, cheers!',
+            next: 'state_tp_start'
+        });
+    }
+
+};
+
 go.app = function() {
     var vumigo = require('vumigo_v02');
     var App = vumigo.App;
-    var Choice = vumigo.states.Choice;
-    var ChoiceState = vumigo.states.ChoiceState;
-    var EndState = vumigo.states.EndState;
 
     var GoApp = App.extend(function(self) {
-        App.call(self, 'states:start');
+        App.call(self, 'state_lp_start');
 
-        self.states.add('states:start', function(name) {
-            return new ChoiceState(name, {
-                question: 'Hi there! What do you want to do?',
 
-                choices: [
-                    new Choice('states:start', 'Show this menu again'),
-                    new Choice('states:end', 'Exit')],
 
-                next: function(choice) {
-                    return choice.value;
-                }
-            });
+        // LEARNER PERFORMANCE STATES
+        // --------------------------
+
+        self.states.add('state_lp_start', function(name) {
+            return go.lp.state_lp_start(name);
         });
 
-        self.states.add('states:end', function(name) {
-            return new EndState(name, {
-                text: 'Thanks, cheers!',
-                next: 'states:start'
-            });
+        self.states.add('state_lp_next', function(name) {
+            return go.lp.state_lp_next(name);
         });
+
+        self.states.add('state_lp_exit', function(name) {
+            return go.lp.state_lp_exit(name);
+        });
+
+
+
+        // TEACHER PERFORMANCE STATES
+        // --------------------------
+
+        self.states.add('state_tp_start', function(name) {
+            return go.tp.state_tp_start(name);
+        });
+
+        self.states.add('state_tp_next', function(name) {
+            return go.tp.state_tp_next(name);
+        });
+
+        self.states.add('state_tp_exit', function(name) {
+            return go.tp.state_tp_exit(name);
+        });
+
     });
 
     return {
