f8de94f89ca30d27121a740d931de6b0e6a29a3a
gsvr <george@westerncapelabs.com>
2014-09-17 11:02:51 +0200

    compiled file

--- go-app.js
+++ go-app.js
@@ -11,6 +11,114 @@ var EndState = vumigo.states.EndState;
 var Choice = vumigo.states.Choice;
 
 
+go.rht = {
+    // Registration of Head Teacher States
+
+    state_rht_start: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('next', 'Go to next state'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'next') {
+                    return 'state_rht_exit';
+                } else {
+                    return 'state_rht_exit';
+                }
+            }
+        });
+    },
+
+    state_rht_exit: function(name) {
+        return new EndState(name, {
+            text: 'Thanks, cheers!',
+            next: 'state_rht_start'
+        });
+    }
+
+};
+
+var vumigo = require('vumigo_v02');
+var ChoiceState = vumigo.states.ChoiceState;
+var EndState = vumigo.states.EndState;
+var Choice = vumigo.states.Choice;
+
+
+go.rdo = {
+    // Registration of Head Teacher States
+
+    state_rdo_start: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('next', 'Go to next state'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'next') {
+                    return 'state_rdo_exit';
+                } else {
+                    return 'state_rdo_exit';
+                }
+            }
+        });
+    },
+
+    state_rdo_exit: function(name) {
+        return new EndState(name, {
+            text: 'Thanks, cheers!',
+            next: 'state_rdo_start'
+        });
+    }
+
+};
+
+var vumigo = require('vumigo_v02');
+var ChoiceState = vumigo.states.ChoiceState;
+var EndState = vumigo.states.EndState;
+var Choice = vumigo.states.Choice;
+
+
+go.cm = {
+    // Registration of Head Teacher States
+
+    state_cm_start: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('next', 'Go to next state'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'next') {
+                    return 'state_cm_exit';
+                } else {
+                    return 'state_cm_exit';
+                }
+            }
+        });
+    },
+
+    state_cm_exit: function(name) {
+        return new EndState(name, {
+            text: 'Thanks, cheers!',
+            next: 'state_cm_start'
+        });
+    }
+
+};
+
+var vumigo = require('vumigo_v02');
+var ChoiceState = vumigo.states.ChoiceState;
+var EndState = vumigo.states.EndState;
+var Choice = vumigo.states.Choice;
+
+
 go.lp = {
     // LearnerPerformance States
 
@@ -113,12 +221,105 @@ go.tp = {
 
 };
 
+var vumigo = require('vumigo_v02');
+var ChoiceState = vumigo.states.ChoiceState;
+var EndState = vumigo.states.EndState;
+var Choice = vumigo.states.Choice;
+
+
+go.sp = {
+    // LearnerPerformance States
+
+    state_sp_start: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('next', 'Go to next state'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'next') {
+                    return 'state_sp_next';
+                } else {
+                    return 'state_sp_exit';
+                }
+            }
+        });
+    },
+
+    state_sp_next: function(name) {
+        return new ChoiceState(name, {
+            question: 'Hi there! What do you want to do?',
+
+            choices: [
+                new Choice('to_tp', 'Switch to TeacherPerformance'),
+                new Choice('exit', 'Exit')],
+
+            next: function(choice) {
+                if(choice.value === 'to_tp') {
+                    return 'state_tp_start'; // Switch to TP
+                } else {
+                    return 'state_sp_exit';
+                }
+            }
+        });
+    },
+
+    state_sp_exit: function(name) {
+        return new EndState(name, {
+            text: 'Thanks, cheers!',
+            next: 'state_sp_start'
+        });
+    }
+
+};
+
 go.app = function() {
     var vumigo = require('vumigo_v02');
     var App = vumigo.App;
 
     var GoApp = App.extend(function(self) {
         App.call(self, 'state_lp_start');
+
+
+
+        // REGISTER HEAD TEACHER STATES
+        // ----------------------------
+
+        self.states.add('state_rht_start', function(name) {
+            return go.rht.state_rht_start(name);
+        });
+
+        self.states.add('state_rht_exit', function(name) {
+            return go.rht.state_rht_exit(name);
+        });
+
+
+
+        // REGISTER DISTRICT OFFICIAL STATES
+        // ---------------------------------
+
+        self.states.add('state_rdo_start', function(name) {
+            return go.rdo.state_rdo_start(name);
+        });
+
+        self.states.add('state_rdo_exit', function(name) {
+            return go.rdo.state_rdo_exit(name);
+        });
+
+
+
+        // CHANGE MANAGEMENT STATES
+        // ------------------------
+
+        self.states.add('state_cm_start', function(name) {
+            return go.cm.state_cm_start(name);
+        });
+
+        self.states.add('state_cm_exit', function(name) {
+            return go.cm.state_cm_exit(name);
+        });
 
 
 
@@ -152,6 +353,23 @@ go.app = function() {
 
         self.states.add('state_tp_exit', function(name) {
             return go.tp.state_tp_exit(name);
+        });
+
+
+
+        // SCHOOL PERFORMANCE STATES
+        // --------------------------
+
+        self.states.add('state_sp_start', function(name) {
+            return go.sp.state_sp_start(name);
+        });
+
+        self.states.add('state_sp_next', function(name) {
+            return go.sp.state_sp_next(name);
+        });
+
+        self.states.add('state_sp_exit', function(name) {
+            return go.sp.state_sp_exit(name);
         });
 
     });
