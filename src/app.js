4826963932e16859dd7e1ce5c77f428078647f07
gsvr <george@westerncapelabs.com>
2014-09-17 11:02:08 +0200

    initiate dummy states

--- src/app.js
+++ src/app.js
@@ -7,6 +7,45 @@ go.app = function() {
 
 
 
+        // REGISTER HEAD TEACHER STATES
+        // ----------------------------
+
+        self.states.add('state_rht_start', function(name) {
+            return go.rht.state_rht_start(name);
+        });
+
+        self.states.add('state_rht_exit', function(name) {
+            return go.rht.state_rht_exit(name);
+        });
+
+
+
+        // REGISTER DISTRICT OFFICIAL STATES
+        // ---------------------------------
+
+        self.states.add('state_rdo_start', function(name) {
+            return go.rdo.state_rdo_start(name);
+        });
+
+        self.states.add('state_rdo_exit', function(name) {
+            return go.rdo.state_rdo_exit(name);
+        });
+
+
+
+        // CHANGE MANAGEMENT STATES
+        // ------------------------
+
+        self.states.add('state_cm_start', function(name) {
+            return go.cm.state_cm_start(name);
+        });
+
+        self.states.add('state_cm_exit', function(name) {
+            return go.cm.state_cm_exit(name);
+        });
+
+
+
         // LEARNER PERFORMANCE STATES
         // --------------------------
 
@@ -37,6 +76,23 @@ go.app = function() {
 
         self.states.add('state_tp_exit', function(name) {
             return go.tp.state_tp_exit(name);
+        });
+
+
+
+        // SCHOOL PERFORMANCE STATES
+        // --------------------------
+
+        self.states.add('state_sp_start', function(name) {
+            return go.sp.state_sp_start(name);
+        });
+
+        self.states.add('state_sp_next', function(name) {
+            return go.sp.state_sp_next(name);
+        });
+
+        self.states.add('state_sp_exit', function(name) {
+            return go.sp.state_sp_exit(name);
         });
 
     });
