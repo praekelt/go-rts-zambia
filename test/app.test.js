19a85faf166331173317e58547a509d3cc02b573
gsvr <george@westerncapelabs.com>
2014-09-26 15:49:00 +0200

    outline tests for learnerperfomance

--- test/app.test.js
+++ test/app.test.js
@@ -279,6 +279,473 @@ describe("when an unregistered user logs on", function() {
 
 
 
+// MONITORING
+// ----------
+
+describe("when a registered user logs on", function() {
+
+    describe("when the registered user is a district official", function() {
+        it("should ask them what they want to do", function() {
+            return tester
+                .setup.user.addr('097444')
+                .inputs('start')
+                .check.interaction({
+                    state: 'initial_state',
+                    reply: [
+                        'What would you like to do?',
+                        '1. Report on teacher performance.',
+                        '2. Report on learner performance',
+                    ].join('\n')
+                })
+                .run();
+        });
+    });
+
+    describe("when the registered user is a head teacher", function() {
+        it("should ask them what they want to do", function() {
+            return tester
+                .setup.user.addr('097555')
+                .inputs('start')
+                .check.interaction({
+                    state: 'initial_state',
+                    reply: [
+                        'What would you like to do?',
+                        '1. Report on teacher performance.',
+                        '2. Report on learner performance',
+                        '3. Change my school',
+                        "4. Update my school's registration data."
+                    ].join('\n')
+                })
+                .run();
+        });
+    });
+
+
+    // LEARNER PERFORMANCE MONITORING
+    // ------------------------------
+
+    describe("when the user chooses to report on learner performance", function() {
+
+        describe("if the user is a district official", function() {
+            it("should ask for an emis code", function() {
+
+            });
+
+            describe("when the district official user enters an emis", function() {
+
+                describe("if the emis does not validate", function() {
+                    it("should ask for the emis again", function() {
+
+                    });
+                });
+
+                describe("if the emis validates", function() {
+                    it("should ask for boys total", function() {
+
+                    });
+
+                    it("should save certain data to the district official user's contact", function() {
+
+                    });
+                });
+
+            });
+
+            // test one full registration as a district official to check data saving
+            describe("when the district official user completes a successful report", function() {
+                it("should ask if they want to return to main menu or exit", function() {
+
+                });
+                
+                it("should save all the data", function() {
+
+                });
+            });
+
+        });
+
+        describe("if the user is a head teacher", function() {
+            it("should ask for boys total", function() {
+
+            });
+
+            // test step by step flow as a head teacher
+
+            // boys total
+            describe("when the user enters boys total", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for boys outstanding results", function() {
+
+                    });
+                });
+
+                // test for numeric value
+                describe("if the number does not validate", function() {
+                    it("should ask for boys total again", function() {
+
+                    });
+                });
+            });
+
+
+            // boys outstanding
+            describe("when the user enters boys outstanding results", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for boys desirable results", function() {
+
+                    });
+                });
+
+                describe("if boys outstanding results > boys total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to boys total", function() {
+
+                    });
+                });
+            });
+
+            // boys desirable
+            describe("when the user enters boys desirable results", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for boys minimum results", function() {
+
+                    });
+                });
+
+                describe("if boys outstanding + desirable > boys total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to boys total", function() {
+
+                    });
+                });
+            });
+
+            // boys minimum
+            describe("when the user enters boys minimum results", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for boys below minimum results", function() {
+
+                    });
+                });
+
+                // test for numeric value
+                describe("if the number does not validate", function() {
+                    it("should ask for boys minimum results again", function() {
+
+                    });
+                });
+
+                describe("if boys outstanding + desirable + minimum > boys total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to boys total", function() {
+
+                    });
+                });
+            });
+
+            // boys below minimum
+            describe("when the user enters boys below minimum results", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls total", function() {
+
+                    });
+                });
+
+                describe("if boys outstanding + desirable + minimum + below minimum > boys total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to boys total", function() {
+
+                    });
+                });
+
+                describe("if boys outstanding + desirable + minimum + below minimum < boys total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to boys total", function() {
+
+                    });
+                });
+            });
+
+
+            // girls total
+            describe("when the user enters girls total", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls outstanding results", function() {
+
+                    });
+                });
+
+
+
+            // girls outstanding
+            describe("when the user enters girls outstanding results", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls desirable results", function() {
+
+                    });
+                });
+
+                describe("if girls outstanding results > girls total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to girls total", function() {
+
+                    });
+                });
+            });
+
+            // girls desirable
+            describe("when the user enters girls desirable results", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls minimum results", function() {
+
+                    });
+                });
+
+                describe("if girls outstanding + desirable > girls total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to girls total", function() {
+
+                    });
+                });
+            });
+
+            // girls minimum
+            describe("when the user enters girls minimum results", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls below minimum results", function() {
+
+                    });
+                });
+
+                describe("if girls outstanding + desirable + minimum > girls total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to girls total", function() {
+
+                    });
+                });
+            });
+
+            // girls below minimum
+            describe("when the user enters girls below minimum results", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for boys phonics", function() {
+
+                    });
+                });
+
+                describe("if girls outstanding + desirable + minimum + below minimum > girls total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to girls total", function() {
+
+                    });
+                });
+
+                describe("if girls outstanding + desirable + minimum + below minimum < girls total", function() {
+                    it("should display error message", function() {
+
+                    });
+
+                    it("should go back to girls total", function() {
+
+                    });
+                });
+            });
+
+
+            // boys phonics
+            describe("when the user enters boys phonics result", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls phonics result", function() {
+
+                    });
+                });
+
+                describe("if boys phonics result > boys total", function() {
+                    it("should ask for boys phonics again", function() {
+
+                    });
+                });
+            });
+
+            // girls phonics
+            describe("when the user enters girls phonics result", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for boys vocab result", function() {
+
+                    });
+                });
+
+                describe("if girls phonics result > girls total", function() {
+                    it("should ask for girls phonics again", function() {
+
+                    });
+                });
+            });
+
+
+            // boys vocab
+            describe("when the user enters boys vocab result", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls vocab result", function() {
+
+                    });
+                });
+
+                describe("if boys vocab result > boys total", function() {
+                    it("should ask for boys vocab again", function() {
+
+                    });
+                });
+            });
+
+            // girls vocab
+            describe("when the user enters girls vocab result", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for boys comprehension result", function() {
+
+                    });
+                });
+
+                describe("if girls vocab result > girls total", function() {
+                    it("should ask for girls vocab again", function() {
+
+                    });
+                });
+            });
+
+
+            // boys comprehension
+            describe("when the user enters boys comprehension result", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls comprehension result", function() {
+
+                    });
+                });
+
+                describe("if boys comprehension result > boys total", function() {
+                    it("should ask for boys comprehension again", function() {
+
+                    });
+                });
+            });
+
+            // girls comprehension
+            describe("when the user enters girls comprehension result", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for boys writing result", function() {
+
+                    });
+                });
+
+                describe("if girls comprehension result > girls total", function() {
+                    it("should ask for girls comprehension again", function() {
+
+                    });
+                });
+            });
+
+
+            // boys writing
+            describe("when the user enters boys writing result", function() {
+                
+                describe("if the number validates", function() {
+                    it("should ask for girls writing result", function() {
+
+                    });
+                });
+
+                describe("if boys writing result > boys total", function() {
+                    it("should ask for boys writing again", function() {
+
+                    });
+                });
+            });
+
+            // girls writing
+            describe("when the user enters girls writing result", function() {
+                
+                describe("if the number validates", function() {
+                    it("should show success and options", function() {
+
+                    });
+
+                    it("should save learner performance data", function() {
+
+                    });
+                });
+
+                describe("if girls writing result > girls total", function() {
+                    it("should ask for girls writing again", function() {
+
+                    });
+                });
+            });
+
+            // completed - main menu
+            describe("after completing report if user selects to go to main menu", function() {
+                it("should go back to main menu", function() {
+
+                });
+            });
+
+            // completed - exit
+            describe("after completing report if user selects to exit", function() {
+                it("should thank them and exit", function() {
+
+                });
+            });
+
+
+        });
+
+    });
+
+});
+
+
 // OTHER
 // -----
 
